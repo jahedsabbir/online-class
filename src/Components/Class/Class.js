853b4c3bd62cc5ea6e381369b90428cc8ccc5f02
jahedsabbir <jahed.sabbir.bd@gmail.com>
2020-08-27 20:11:54 +0600

    final commit

--- src/Components/Class/Class.js
+++ src/Components/Class/Class.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Class.css'
+
 const Class = (props) => {
     console.log(props);
     const {title, price, img} = props.courses;
@@ -10,7 +11,8 @@ const Class = (props) => {
             <img src={img} alt=""/>
             <h2>{title}</h2>
             <h3>Price : {price}</h3>
-            <button onClick ={() => props.addCourse(props.courses)}>Enrol Now</button>
+            <button  onClick ={() => props.addCourse(props.courses)}>Enroll Now</button>
+            
         </div>
     );
 };
